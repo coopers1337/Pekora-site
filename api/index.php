5b5cd2289d25aa6e7eedcb5a399a56bd857c4dd3
Cooper <admin@coopers1337.xyz>
2025-06-11 16:21:22 +0900

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,21 +1,30 @@
 <?php
 header('Content-Type: application/json');
 
-function fetchUser($userId) {
+// Load environment variables (for .ROBLOSECURITY cookie)
+$roblosecurity = getenv('ROBLOSECURITY') ?: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzZXNzaW9uSWQiOiJhMmEyNzI1Ni1mZjg2LTQ2YmItODk0MC0yYzg3OTEzMzUzODYiLCJjcmVhdGVkQXQiOjE3NDc0NjgxNjZ9.4ii56b27JbjzpVf23vMrBOGUaCXysf4ZruT27E_WAhGxSlUS78Dag36UGjLnrYXqfLdHnZZRzyKyH4ROjrIlvQ';
+
+function fetchUser($userId, $roblosecurity) {
     $url = "https://www.pekora.zip/apisite/users/v1/users/$userId";
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/json']);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Accept: application/json',
+        "Cookie: .ROBLOSECURITY=$roblosecurity"
+    ]);
     $response = curl_exec($ch);
     curl_close($ch);
     return json_decode($response, true);
 }
 
-function fetchAvatar($userId) {
+function fetchAvatar($userId, $roblosecurity) {
     $url = "https://www.pekora.zip/apisite/thumbnails/v1/users/avatar-headshot?userIds=$userId&size=420x420&format=png";
     $ch = curl_init($url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/json']);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Accept: application/json',
+        "Cookie: .ROBLOSECURITY=$roblosecurity"
+    ]);
     $response = curl_exec($ch);
     curl_close($ch);
     $data = json_decode($response, true);
@@ -26,12 +35,12 @@ $latestUserId = 10900;
 $nextUserId = 10901;
 
 // Fetch latest user (10900)
-$latestUserResponse = fetchUser($latestUserId);
-$latestAvatarResponse = fetchAvatar($latestUserId);
+$latestUserResponse = fetchUser($latestUserId, $roblosecurity);
+$latestAvatarResponse = fetchAvatar($latestUserId, $roblosecurity);
 
 // Fetch next user (10901)
-$nextUserResponse = fetchUser($nextUserId);
-$nextAvatarResponse = fetchAvatar($nextUserId);
+$nextUserResponse = fetchUser($nextUserId, $roblosecurity);
+$nextAvatarResponse = fetchAvatar($nextUserId, $roblosecurity);
 
 // Prepare response
 $response = [
